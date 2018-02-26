b55c98854521968377de769bbca53c9a96c0dac4
Arvind Kushwah <arvindkushwah9@gmail.com>
2018-02-26 13:28:30 +0530

    changes for db config

--- app/models/concerns/tracadmin_db.rb
+++ app/models/concerns/tracadmin_db.rb
@@ -1,17 +1,17 @@
 module TracadminDb
   extend ActiveSupport::Concern
 
-  # included do
-	 #  self.establish_connection(
-		# 	adapter: ENV["DB_tracadmin_adapter"],
-		# 	encoding: ENV["DB_tracadmin_encoding"],
-		# 	reconnect: ENV["DB_tracadmin_reconnect"],
-		# 	pool: ENV["DB_tracadmin_pool"],
-		# 	secure_auth: ENV["DB_tracadmin_secure_auth"],
-		# 	database: ENV["DB_tracadmin_database"],
-		# 	username: ENV["DB_tracadmin_username"],
-		# 	password: ENV["DB_tracadmin_password"],
-		# 	host: ENV["DB_tracadmin_host"],
-		# 	port: ENV["DB_tracadmin_port"])
-  # end
+  included do
+	  self.establish_connection(
+			adapter: ENV["DB_tracadmin_adapter"],
+			encoding: ENV["DB_tracadmin_encoding"],
+			reconnect: ENV["DB_tracadmin_reconnect"],
+			pool: ENV["DB_tracadmin_pool"],
+			secure_auth: ENV["DB_tracadmin_secure_auth"],
+			database: ENV["DB_tracadmin_database"],
+			username: ENV["DB_tracadmin_username"],
+			password: ENV["DB_tracadmin_password"],
+			host: ENV["DB_tracadmin_host"],
+			port: ENV["DB_tracadmin_port"])
+  end
 end
